b00d64aa4fd7cdd718443b5437b7b3264a3700f7
FoamAngel32 <bilibililaiabao55@126.com>
2026-05-07 14:06:12 +0800

    Update link.bat

--- build/link.bat
+++ build/link.bat
@@ -1,4 +1,4 @@
-arm-none-eabi/bin/ar rcs frames.a img_T*.o
-arm-none-eabi-gcc -specs=gba.specs soundbank.bin.o bad_apple_palette.o main.o frames.a -o SoftwareGBAVideo.elf -LF:/devkitpro/msys2/opt/devkitpro/libgba/lib -lmm -lgba 
-arm-none-eabi-objcopy -O binary SoftwareGBAVideo.elf SoftwareGBAVideo.gba
-gbafix SoftwareGBAVideo.gba -tno -c0000 -m00
+F:/devkitPro/msys2/opt/devkitpro/devkitARM/arm-none-eabi/bin/ar rcs frames.a img_T*.o
+F:/devkitPro/msys2/opt/devkitpro/devkitARM/bin/arm-none-eabi-gcc -specs=gba.specs soundbank.bin.o palette.o main.o frames.a -o SoftwareGBAVideo.elf -LF:/devkitpro/msys2/opt/devkitpro/libgba/lib -lmm -lgba 
+F:/devkitPro/msys2/opt/devkitpro/devkitARM/bin/arm-none-eabi-objcopy -O binary SoftwareGBAVideo.elf SoftwareGBAVideo.gba
+F:/devkitPro/msys2/opt/devkitpro/tools/bin/gbafix SoftwareGBAVideo.gba -tno -c0000 -m00
