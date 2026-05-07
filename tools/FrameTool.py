bfb741c7ede595410aa5306554b655e4bce995d2
FoamAngel32 <bilibililaiabao55@126.com>
2026-05-07 13:35:20 +0800

    Update FrameTool.py

--- tools/FrameTool.py
+++ tools/FrameTool.py
@@ -1,6 +1,6 @@
 num_frames = abs(int(float((''.join(filter(str.isdigit, input("how many frame do you have?\n>>>")) or '0')))))
 name = input("What name are they all start with?(must end with 4 digits of number index and start with 0)\n>>>")
-with open("F:/project/GBA/SoftwareGBAVideo/src/frames.h", "w") as f:
+with open("../src/frames.h", "w") as f:
     f.write("#ifndef FRAMES2_H\n#define FRAMES2_H\n\n")
     for i in range(num_frames):
         f.write(f'#include "{name}{i:04d}.h"\n')
